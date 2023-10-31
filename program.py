9e61f4e457663143f6ebc822b663a0124ac68f38
Harivardhan Pyaram (PRFT) <hpyaram@commonwealthcare.org>
2023-10-31 11:11:00 -0400

    Fixing variables part two

--- program.py
+++ program.py
@@ -1,15 +1,16 @@
 # This script is only simulating a deployment script running in automated deployments.
 
-print("This is a simulation of deployment script written by Harivardhan.\n")
-print("It accesses secrets and variables defined for the environment in Repo Settings as follows:\n\n")
+print("This is a simulation of deployment script written by Harivardhan.")
+print("It accesses secrets and variables defined for the environment in Repo Settings as follows:\n")
 
-print("DB_URL: ${{ vars.DB_URL }}\n")
-print("DB_USERNAME: ${{ vars.DB_USERNAME }}\n")
-print("DB_PASSWORD: ${{ vars.DB_PASSWORD }}\n")
+print("DB_URL: $(DB_URL)")
+print("DB_USERNAME: $(DB_USERNAME)")
+print("DB_PASSWORD: $(DB_PASSWORD)")
 
-print("Deployment completed.\n")
+print("Deployment completed.")
 
 print("------------------------------------------------------------------------------------------------------------------")
 print("Initial Commit - 10/30")
 
 print("Fixing variables and adding parenthesis - 10/31")
+print("Fixing variables part two - 10/31")
