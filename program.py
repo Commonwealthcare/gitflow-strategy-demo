ba1beca2d1dbb984ff04731fbb7217bd95111a1f
Harivardhan Pyaram (PRFT) <hpyaram@commonwealthcare.org>
2023-10-31 11:18:05 -0400

    Accessing env variables

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 # This script is only simulating a deployment script running in automated deployments.
 
+import os
+
 print("This is a simulation of deployment script written by Harivardhan.")
 print("It accesses secrets and variables defined for the environment in Repo Settings as follows:\n")
 
-print("DB_URL: $(DB_URL)")
-print("DB_USERNAME: $(DB_USERNAME)")
-print("DB_PASSWORD: $(DB_PASSWORD)")
+print("DB_URL: os.getenv(DB_URL)")
+print("DB_USERNAME: os.getenv(DB_USERNAME)")
+print("DB_PASSWORD: os.getenv(DB_PASSWORD)")
 
 print("Deployment completed.")
 
@@ -14,3 +16,4 @@ print("Initial Commit - 10/30")
 
 print("Fixing variables and adding parenthesis - 10/31")
 print("Fixing variables part two - 10/31")
+print("Accessing env variables - 10/31")
