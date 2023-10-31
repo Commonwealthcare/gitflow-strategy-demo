44e06deae391d12c23dc43aa46bbf08b7ecd0722
prftharivrdhn <144474855+prftharivrdhn@users.noreply.github.com>
2023-10-30 22:18:30 -0400

    Create program.py
    Adding the deploy script..

---
+++ program.py
@@ -0,0 +1,13 @@
+# This script is only simulating a deployment script running in automated deployments.
+
+print "This is a simulation of deployment script written by Harivardhan.\n"
+print "It accesses secrets and variables defined for the environment in Repo Settings as follows:\n\n"
+
+print "DB_URL: $(DB_URL)\n"
+print "DB_USERNAME: $(DB_USERNAME)\n"
+print "DB_PASSWORD: $(DB_PASSWORD)\n"
+
+print "Deployment completed.\n"
+
+print "------------------------------------------------------------------------------------------------------------------"
+print "Initial Commit - 10/30"
