9ce1450203ddff175fea5abd64d154f1652dc909
Harivardhan Pyaram (PRFT) <hpyaram@commonwealthcare.org>
2023-10-31 11:35:17 -0400

    this should work

--- program.py
+++ program.py
@@ -5,9 +5,9 @@ import os
 print("This is a simulation of deployment script written by Harivardhan.")
 print("It accesses secrets and variables defined for the environment in Repo Settings as follows:\n")
 
-DB_URL = os.getenv(DB_URL)
-DB_USERNAME = os.getenv(DB_USERNAME)
-DB_PASSWORD = os.getenv(DB_PASSWORD)
+DB_URL = os.getenv('DB_URL')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 
 print("DB_URL: $(DB_URL)")
